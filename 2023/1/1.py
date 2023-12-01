d8924916b6bf688cc0b9428db48fca7799171e1f
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2023-11-30 23:08:22 -0500

    base for 2023 beginning

---
+++ 2023/1/1.py
@@ -0,0 +1,21 @@
+import time
+
+
+def solution():
+    data = open(r'2023\1\1.in').readlines()
+    # data = open(r'2023\1\ex.in').readlines()
+    data = [x.strip() for x in data]
+    start = time.time()
+    print('Part 1 result: ' + str(part1(data)))
+    print(f'Finished p1 in {time.time() - start} seconds')
+    start = time.time()
+    print('Part 2 result: ' + str(part2(data)))
+    print(f'Finished p2 in {time.time() - start} seconds')
+    
+def part1(data):
+    return 'Not impl'
+
+def part2(data):
+    return 'Not impl'
+
+solution()
