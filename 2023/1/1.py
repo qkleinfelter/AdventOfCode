f32db1340e6d9b4a8710b40559066d8e55adf9dd
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2023-12-01 00:10:51 -0500

    d1p1

--- 2023/1/1.py
+++ 2023/1/1.py
@@ -6,16 +6,44 @@ def solution():
     # data = open(r'2023\1\ex.in').readlines()
     data = [x.strip() for x in data]
     start = time.time()
-    print('Part 1 result: ' + str(part1(data)))
-    print(f'Finished p1 in {time.time() - start} seconds')
+    # print('Part 1 result: ' + str(part1(data)))
+    # print(f'Finished p1 in {time.time() - start} seconds')
     start = time.time()
     print('Part 2 result: ' + str(part2(data)))
     print(f'Finished p2 in {time.time() - start} seconds')
     
 def part1(data):
-    return 'Not impl'
+    sum = 0
+    for line in data:
+        num = ''
+        for char in line:
+            if char in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'):
+                num += char
+        num = int(num[0] + num[-1])
+        sum += num
+    return sum
 
+mapping = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9
+}
 def part2(data):
-    return 'Not impl'
+    sum = 0
+    for line in data:
+        num = ''
+        for char in line:
+            if char in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'):
+                num += char
+        num = int(num[0] + num[-1])
+        sum += num
+    return sum
 
 solution()
